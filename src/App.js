34c74b18182ad9c24ba937371420d6b8dac671f3
Clint Riggan <clint.riggan@abcfinancial.com>
2022-02-04 14:30:27 -0600

    Changed App page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World.  This is me!
         </a>
       </header>
     </div>
